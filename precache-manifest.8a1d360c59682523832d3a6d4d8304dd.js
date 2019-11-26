625f6c2e5873fa1314520f27d2a7c91d645f4eee
Abhishek Dasgupta <abhishek.dasgupta@target.com>
2019-11-26 10:01:12 -0600

    Updates

---
+++ precache-manifest.8a1d360c59682523832d3a6d4d8304dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afb75322ed0faa97c2509eb60d92b2cc",
+    "url": "/shopit/static/media/HelveticaNeue.afb75322.woff"
+  },
+  {
+    "revision": "18206aa9686cb1c02e93",
+    "url": "/shopit/static/js/runtime~main.18206aa9.js"
+  },
+  {
+    "revision": "6349835f68515a6b75b5",
+    "url": "/shopit/static/js/main.6349835f.chunk.js"
+  },
+  {
+    "revision": "113dcefcacf212c31967",
+    "url": "/shopit/static/js/1.113dcefc.chunk.js"
+  },
+  {
+    "revision": "6349835f68515a6b75b5",
+    "url": "/shopit/static/css/main.ba4e615c.chunk.css"
+  },
+  {
+    "revision": "5810755e1641ab9b2a8f892c54e6aa68",
+    "url": "/shopit/index.html"
+  }
+];
